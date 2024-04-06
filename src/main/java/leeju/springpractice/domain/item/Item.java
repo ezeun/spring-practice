1e56f529ed9cead9352a9777b1d5469a9d6fb68a
ezeun <consistency1231@gmail.com>
2024-04-06 21:58:03 +0900

    상품 도메인 개발

--- src/main/java/leeju/springpractice/domain/item/Item.java
+++ src/main/java/leeju/springpractice/domain/item/Item.java
@@ -2,6 +2,7 @@ package leeju.springpractice.domain.item;
 
 import jakarta.persistence.*;
 import leeju.springpractice.domain.Category;
+import leeju.springpractice.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,4 +25,16 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<Category>();
+
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if(restStock<0){
+            throw new NotEnoughStockException("재고 부족");
+        }
+        this.stockQuantity = restStock;
+    }
 }
